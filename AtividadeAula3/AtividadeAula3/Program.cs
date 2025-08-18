ddba408c9de84b6b7d7b33fdba07c6b9f291bdfc
Pedro Henrique de Assunção Lima <143238314+phz1n777@users.noreply.github.com>
2025-08-18 11:33:17 -0300

    Aula 3 e Atividade Operações e Condicionais

---
+++ AtividadeAula3/AtividadeAula3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade1, idade2;
+            Console.WriteLine("Insira a idade do aluno 1:");
+            idade1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a idade do aluno 2:");
+            idade2 = int.Parse(Console.ReadLine());
+
+            if (idade1 == idade2)
+            {
+                Console.WriteLine("Os dois alunos tem a mesma idade.");
+            }
+
+            if (idade1 > idade2)
+            {
+                Console.WriteLine("O aluno 1 é mais velho e o aluno 2 é mais novo.");
+            }
+
+            if (idade1 < idade2)
+            {
+                Console.WriteLine("O aluno 1 é mais novo e o aluno 2 é mais velho.");
+            }
+        }
+    }
+}
